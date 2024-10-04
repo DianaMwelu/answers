73c67a92b0d32de26f3372a3f8f73edc0e158cc7
root <root@localhost>
2024-10-04 09:15:53 +0300

    auto publish 1728022553606

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int Book_ID;
+    int Due_Date;
+    int Return_Date;
+    printf("enter the value of Book_ID:");
+    scanf("%d",&Book_ID);
+    printf("the value is %d\n",Book_ID);
+    
+    printf("enter the value of Due_Date:");
+    scanf("%d",&Due_Date);
+    printf("the value is %d\n",Due_Date);
+    
+    printf("enter the value of Return_Date:");
+    scanf("%d",&Return_Date);
+    printf("the value is %d\n",Return_Date);
+    return 0;
+}
